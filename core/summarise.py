065a62faf11925c2e0b62ed6cd6a00886daaccdd
aditya gupta <coonect2adityagupta@gmail.com>
2026-08-16 13:35:09 +0530

    Add summarizer file

---
+++ core/summarise.py
@@ -0,0 +1,64 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnableLambda,RunnablePassthrough
+from dotenv import load_dotenv
+load_dotenv()
+import os 
+def getllm():
+    return ChatMistralAI(model="mistral-small-latest",temperature=0.2)
+def getllm2():
+    return ChatGroq(model="openai/gpt-oss-20b", temperature=0.2).with_retry(
+        stop_after_attempt=3,
+        wait_exponential_jitter=True,
+    )
+
+
+def split_transcript(transcript:str)->list:
+    splitter=RecursiveCharacterTextSplitter(
+        chunk_size=10000,
+        chunk_overlap=890,
+    )
+    return splitter.split_text(transcript)
+
+def summarize(transcript:str)->str:
+    llm=getllm()
+    map_prompt=ChatPromptTemplate.from_messages(
+        [
+            ("system","Summarize this portion of a meeting transcript concisely and only in english"),
+            ("human","{text}"),
+        ]
+    )
+    map_chain=map_prompt|llm|StrOutputParser()# summarize each chunk section of a video
+    chunks=split_transcript(transcript) 
+    chunk_summaries=[map_chain.invoke({"text":chunk})for chunk in chunks]
+    combined="\n\n".join(chunk_summaries)
+    # summaries may overlapp so we will sumaarise whole finally
+    combined_prompt=ChatPromptTemplate.from_messages([
+        ("system","You are a expert summariser .combine these partial summaries into a complete professional meeting summary in bullet points ."),
+        ("human","{text}"),
+    ])
+    combined_chain = combined_prompt | llm | StrOutputParser()
+
+    # Recursive reduce: keep summarizing chunks until we have one final chunk that fits in the context window
+    combined_chunks = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=200).split_text(combined)
+    
+    while len(combined_chunks) > 1:
+        reduced_summaries = [combined_chain.invoke({"text": chunk}) for chunk in combined_chunks]
+        new_combined = "\n\n".join(reduced_summaries)
+        combined_chunks = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=200).split_text(new_combined)
+        
+    return combined_chain.invoke({"text": combined_chunks[0]})
+
+def generate_title(transcript:str)->str:
+    llm=getllm2()
+    title_prompt=ChatPromptTemplate.from_messages([
+        ("system","Based on the meeting transcript, generate a short professional meeting title "
+                "(max 8 words). Only return the title, nothing else."),
+        ("human","{text}")
+    ])
+    text=transcript[:2000]
+    title_chain=title_prompt|llm|StrOutputParser()
+    return title_chain.invoke({"text":text})

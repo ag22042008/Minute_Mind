924c8322989240b2f1ab5d037e7e2251c5d7f027
aditya gupta <coonect2adityagupta@gmail.com>
2026-08-18 23:53:05 +0530

    Update available Gemini model to 2.5-flash

--- core/extractor.py
+++ core/extractor.py
@@ -60,7 +60,7 @@ def getllm2():
 
 def getllm3():
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash-latest",
+        model="gemini-2.5-flash",
         temperature=0.2,
         rate_limiter=gemini_rate_limiter,
     ).with_retry(stop_after_attempt=3, wait_exponential_jitter=True)

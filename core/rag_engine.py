d39896bb69a76c088a773e3d4fa3cf684fbccc1c
Aditya Gupta <coonect2adityagupta@gmail.com>
2026-09-14 01:21:37 +0530

    Update LLM model and add Groq API key

--- core/rag_engine.py
+++ core/rag_engine.py
@@ -124,6 +124,7 @@
 
 import os 
 from langchain_mistralai import ChatMistralAI
+from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough,RunnableLambda
@@ -132,8 +133,8 @@ from core.vectore_store import load_vector_store,build_vector_store,get_retrieve
 
 def get_llm():
     return ChatMistralAI(
-        model="mistral-small-latest",
-        mistral_api_key=os.getenv("MISTRAL_API_KEY"),
+        model="openai/gpt-oss-120b",
+        groq_api_key=os.getenv("GROQ_API_KEY"),
         temperature=0.25,
         max_tokens=1024
     )
